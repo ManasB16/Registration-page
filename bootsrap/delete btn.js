6c3b2c0ec77846ea2a67482b77f822746891d98f
ManasB16 <manasbhola@gmail.com>
2023-05-05 22:09:06 +0530

    chnged

--- bootsrap/delete btn.js
+++ bootsrap/delete btn.js
@@ -7,31 +7,12 @@ const time = document.querySelector('#time')
 const userlist = document.querySelector('#users')
 
 
+
 form.addEventListener('submit', onsubmit)
 
 
 function onsubmit(e){
     e.preventDefault();
-
-    // console.log(name.value)
-    // console.log(email.value)
-    // console.log(phone.value)
-    // console.log(date.value)
-    // console.log(time.value)
-
-    const li = document.createElement('li')
-    const btn = document.createElement('button')
-    btn.className = 'delete'
-    let btntxt = document.createTextNode('Delete')
-    li.appendChild(document.createTextNode(`${name.value} : ${email.value} - ${phone.value}`))
-    btn.appendChild(btntxt)
-    
-
-    // localStorage.setItem('Name', name.value)
-    // localStorage.setItem('Email', email.value)
-    // localStorage.setItem('Phone', phone.value)
-    // localStorage.setItem('Date', date.value)
-    // localStorage.setItem('Time', time.value)
 
     let obj1 = {
         name : name.value,
@@ -41,30 +22,39 @@ function onsubmit(e){
         time : time.value
     } 
 
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+    const edit = document.createElement('button')
+    let btntxt = document.createTextNode('Delete')
+    let edittxt = document.createTextNode('Edit')
+
+    let text = obj1.name + '-' + obj1.email + '-' + obj1.phone
+
+    li.appendChild(document.createTextNode(text))
+    btn.appendChild(btntxt)
+    edit.appendChild(edittxt)
+
+
+    btn.onclick = (e) => {
+        localStorage.removeItem(obj1.name) 
+        userlist.removeChild(e.target.parentElement)
+    }
+    edit.onclick = (e) => {
+        localStorage.removeItem(obj1.name)
+        userlist.removeChild(e.target.parentElement)
+        document.querySelector('#name').value = obj1.name
+        document.querySelector('#email').value = obj1.email
+        document.querySelector('#phone').value = obj1.phone
+    }
+
+    li.appendChild(btn)
+    li.appendChild(edit)
+    userlist.append(li);
+
     let serialized = JSON.stringify(obj1)
 
     localStorage.setItem(name.value, serialized)
 
-    userlist.append(li);
-    li.appendChild(btn)
-
     // name.value = ''
-    email.value = ''
-    phone.value = ''
-    date.value = ''
-    time.value = ''
 
-}
-
-    let itemlist = document.getElementById('users')
-    itemlist.addEventListener('click', removeitem)
-
-function removeitem(e) {
-    if(e.target.classList.contains('delete')){
-        localStorage.removeItem(name.value)
-        let li = e.target.parentElement;
-        itemlist.removeChild(li)
-        
-    }
-    
 }
